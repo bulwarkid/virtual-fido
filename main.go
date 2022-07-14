40f530648e18e7eec2366a69c385e9c3dbb9037b
Chris de la Iglesia <chris@bulwark.id>
2022-07-14 11:38:34 -0700

    Remove checkEOF calls from the codebase

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 var device FIDODevice
 
 func handleCommandSubmit(conn *net.Conn, header USBIPMessageHeader, command USBIPCommandSubmitBody) {
-	checkEOF(conn)
 	transferBuffer := make([]byte, command.TransferBufferLength)
 	if header.Direction == USBIP_DIR_OUT && command.TransferBufferLength > 0 {
 		_, err := (*conn).Read(transferBuffer)
@@ -16,7 +15,6 @@ func handleCommandSubmit(conn *net.Conn, header USBIPMessageHeader, command USBI
 	}
 	switch command.Setup.BRequest {
 	case USB_REQUEST_GET_DESCRIPTOR:
-		checkEOF(conn)
 		descriptor := device.getDescriptor(command.Setup.WValue)
 		copy(transferBuffer, descriptor)
 		replyHeader, replyBody, _ := newReturnSubmit(header, command, (transferBuffer))
@@ -24,7 +22,6 @@ func handleCommandSubmit(conn *net.Conn, header USBIPMessageHeader, command USBI
 		write(*conn, toBE(replyHeader))
 		write(*conn, toBE(replyBody))
 		write(*conn, transferBuffer)
-		checkEOF(conn)
 	default:
 		panic(fmt.Sprintf("Invalid CMD_SUBMIT bRequest: %d", command.Setup.BRequest))
 	}
@@ -32,10 +29,8 @@ func handleCommandSubmit(conn *net.Conn, header USBIPMessageHeader, command USBI
 
 func handleCommands(conn *net.Conn) {
 	for {
-		checkEOF(conn)
 		header := readBE[USBIPMessageHeader](*conn)
 		fmt.Printf("MESSAGE HEADER: %#v\n\n", header)
-		checkEOF(conn)
 		if header.Command == USBIP_COMMAND_SUBMIT {
 			command := readBE[USBIPCommandSubmitBody](*conn)
 			fmt.Printf("COMMAND SUBMIT: %#v\n\n", command)
@@ -53,7 +48,6 @@ func handleConnection(conn *net.Conn) {
 	for {
 		header := readBE[USBIPControlHeader](*conn)
 		fmt.Printf("USBIP CONTROL MESSAGE: %#v\n\n", header)
-		checkEOF(conn)
 		if header.CommandCode == USBIP_COMMAND_OP_REQ_DEVLIST {
 			reply := newOpRepDevlist()
 			fmt.Printf("OP_REP_DEVLIST: %#v\n\n", reply)
@@ -77,15 +71,10 @@ func main() {
 	fmt.Println("Starting USBIP server...")
 	device = FIDODevice{}
 	listener, err := net.Listen("tcp", ":3240")
-	if err != nil {
-		fmt.Println("Could not create listener:", err)
-		return
-	}
+	checkErr(err, "Could not create listener")
 	for {
 		connection, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Connection error:", err)
-		}
+		checkErr(err, "Connection accept error")
 		handleConnection(&connection)
 	}
 }
