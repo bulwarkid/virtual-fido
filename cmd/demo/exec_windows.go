bb2a67035121615a70320b873384cd9b18f4df6d
Chris de la Iglesia <chris@cmdli.dev>
2023-07-19 15:39:18 -0700

    Update USBIP to latest release

--- cmd/demo/exec_windows.go
+++ cmd/demo/exec_windows.go
@@ -6,5 +6,7 @@ import "os/exec"
 
 // Execute USB IP attach for Windows
 func platformUSBIPExec() *exec.Cmd {
-	return exec.Command("./cmd/demo/usbip/bin/usbip.exe", "attach", "-r", "127.0.0.1", "-b", "2-2")
+	command := exec.Command(".\\usbip.exe", "attach", "-r", "127.0.0.1", "-b", "2-2")
+	command.Dir = ".\\cmd\\demo\\usbip\\bin"
+	return command
 }
